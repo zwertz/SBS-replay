258e61ec632bb82fd5ad9cb74f1f22094079b377
Eric Fuchey <efuchey@jlab.org>
2021-10-12 17:16:09 -0400

    Updated BBCalCalibration.C script: * filling the matrix elements from the blocks contained in the reconstructed main cluster (not all blocks);  => Q: how come the exisiting calibration is applied to clus_blk.e in addition to clus_blk.e_c? bug? if not, why having both? * added the filtering of events where the max of the shower cluster is on a peripheral block; * added the matrix element filtering based on the stats per element; * added the printout of the errors.  => Q: what is supposed to be "Etarg" ? (I saw that from the Hcal calibration script).
    Added a few important parameters in the MC GEM DB.

--- scripts/bbcal/BBCalCalibration.C
+++ scripts/bbcal/BBCalCalibration.C
@@ -9,7 +9,8 @@
 #include "TLegend.h"
 #include "gmn_rec_tree.C"
 
-void BBCalCalibration(const char* filename = "replayed_simdigtest_2.root")
+// filename
+void BBCalCalibration(const char* filename, const int Nmin = 1000, const double minMBratio = 0.1, const int printstat = 0)
 {
   TFile *_file0 = TFile::Open(filename);
   TChain *C = (TChain*)_file0->Get("T");
@@ -25,7 +26,10 @@ void BBCalCalibration(const char* filename = "replayed_simdigtest_2.root")
   //convention: 0-188: SH; 189-240: PS.
   TMatrixD M_jk(241, 241);
   TMatrixD B_j(241, 1);
-    
+  
+  int nevts[241];
+  memset(nevts, 0, 241*sizeof(int));
+  
   double A_j[241];
   
   double E_i = 0;
@@ -71,23 +75,65 @@ void BBCalCalibration(const char* filename = "replayed_simdigtest_2.root")
     h1_DeltaE->Fill(1.-A_i/E_i);
     */
     
-    double x_tr = T->bb_tr_x[imax]+1.95715*T->bb_tr_th[imax];
-    double y_tr = T->bb_tr_y[imax]+1.95715*T->bb_tr_ph[imax];
+    //double x_tr = T->bb_tr_x[imax]+1.95715*T->bb_tr_th[imax];
+    //double y_tr = T->bb_tr_y[imax]+1.95715*T->bb_tr_ph[imax];
     
     //cout << T->bb_tr_x[imax] << " " << T->bb_tr_th[imax] << " " << T->bb_tr_p[imax] << endl;
     
-    for(int i = 0; i<T->bb_sh_ngoodADChits; i++){
-      if(fabs(T->bb_sh_goodblock_x[i]-x_tr)<0.085*2 || fabs(T->bb_sh_goodblock_y[i]-y_tr)<0.085*2)
-	A_j[int(T->bb_sh_goodblock_id[i])]+=T->bb_sh_goodblock_e[i];
+    //if no cluster, skip the event.
+    if(T->bb_sh_nclus==0 || T->bb_ps_nclus==0)continue;
+    
+    
+    int k_max = -1;
+    double nblk = -1;
+    double Emax = -10;
+    // loop on clusters first: select highest energy
+    for(int k = 0; k<T->bb_sh_nclus; k++){
+      if(T->bb_sh_clus_e_c[k]>Emax){
+	Emax = T->bb_sh_clus_e_c[k];
+	k_max = k;
+	nblk = T->bb_sh_clus_nblk[k];
+      }
+    }
+    //reject events with max on the edge
+    if(T->bb_sh_clus_row[k_max]==0 || T->bb_sh_clus_row[k_max]==26 ||
+       T->bb_sh_clus_col[k_max]==0 || T->bb_sh_clus_col[k_max]==6)continue;
+    
+    for(int i = 0; i<nblk;i++){
+      nevts[int(T->bb_sh_clus_blk_id[i])]++;
+      A_j[int(T->bb_sh_clus_blk_id[i])]+=T->bb_sh_clus_blk_e[i];
+    }
+    
+    k_max = -1;
+    nblk = -1;
+    Emax = -10;
+    // loop on clusters first: select highest energy
+    for(int k = 0; k<T->bb_ps_nclus; k++){
+      if(T->bb_ps_clus_e_c[k]>Emax){
+	Emax = T->bb_ps_clus_e_c[k];
+	k_max = k;
+	nblk = T->bb_ps_clus_nblk[k];
+      }
     }
 
-    x_tr = T->bb_tr_x[imax]+1.75045*T->bb_tr_th[imax];
-    y_tr = T->bb_tr_y[imax]+1.75045*T->bb_tr_ph[imax];
-    
-    for(int i = 0; i<T->bb_ps_ngoodADChits; i++){
-      if(fabs(T->bb_ps_goodblock_x[i]-x_tr)<0.09*2 || fabs(T->bb_sh_goodblock_y[i]-y_tr)<0.225)
-	 A_j[189+int(T->bb_ps_goodblock_id[i])]+=T->bb_ps_goodblock_e[i];
+    for(int i = 0; i<nblk;i++){
+      nevts[189+int(T->bb_ps_clus_blk_id[i])]++;
+      A_j[189+int(T->bb_ps_clus_blk_id[i])]+=T->bb_ps_clus_blk_e[i];
     }
+    
+    
+    //for(int i = 0; i<T->bb_sh_ngoodADChits; i++){
+    //if(fabs(T->bb_sh_goodblock_x[i]-x_tr)<0.085*2 || fabs(T->bb_sh_goodblock_y[i]-y_tr)<0.085*2)
+    //A_j[int(T->bb_sh_goodblock_id[i])]+=T->bb_sh_goodblock_e[i];
+    //}
+
+    //x_tr = T->bb_tr_x[imax]+1.75045*T->bb_tr_th[imax];
+    //y_tr = T->bb_tr_y[imax]+1.75045*T->bb_tr_ph[imax];
+    
+    //for(int i = 0; i<T->bb_ps_ngoodADChits; i++){
+    //if(fabs(T->bb_ps_goodblock_x[i]-x_tr)<0.09*2 || fabs(T->bb_sh_goodblock_y[i]-y_tr)<0.225)
+    //A_j[189+int(T->bb_ps_goodblock_id[i])]+=T->bb_ps_goodblock_e[i];
+    //}
     
     for(int j = 0; j<241; j++){
       B_j(j, 0)+= A_j[j]*E_i;
@@ -98,6 +144,64 @@ void BBCalCalibration(const char* filename = "replayed_simdigtest_2.root")
     
   }
   
+  int k = 0;
+  bool badcells[241];
+  if(printstats>0){
+    cout << "Printing counts per element: shower" << endl;
+    for(int i = 0; i<27; i++){
+      for(int j = 0; j<7; j++){
+	cout << nevts[k] << "  ";
+	k++;
+      }
+      cout << endl;
+    }
+    cout << endl;
+    cout << "Printing counts per element: preshower" << endl;
+    for(int i = 0; i<26; i++){
+      for(int j = 0; j<2; j++){
+	cout << nevts[k] << "  ";
+	k++;
+      }
+      cout << endl;
+    }
+    cout << endl;
+    
+    if(printstats>1){
+      cout << "Printing matrix elements: shower" << endl;
+      for(int i = 0; i<27; i++){
+	for(int j = 0; j<7; j++){
+	  cout << M_jk(k,k) << " >=? "<< minMBratio*B_j(k, 0) << endl;
+	  k++;
+	}
+      }
+      cout << endl;  
+      
+      cout << "Printing matrix elements: preshower" << endl;
+      for(int i = 0; i<26; i++){
+	for(int j = 0; j<2; j++){
+	  cout << M_jk(k,k) << " >=? "<< minMBratio*B_j(k, 0) << endl;
+	  k++;
+	}
+      }
+      cout << endl;   
+    }
+  }
+  
+  for(int j = 0; j<241; j++){
+    badcells[j]=false;
+    if(nevts[j]<Nmin || M_jk(j, j)< minMBratio*B_j(j, 0)){
+      B_j(j, 0) = 1.;
+      M_jk(j, j) = 1.;
+      for(int k = 0; k<241; k++){
+	if(k!=j){
+	  M_jk(j, k) = 0.;
+	  M_jk(k, j) = 0.;
+	}
+      }
+      badcells[j]=true;
+    }
+  }
+  
   //B_j.Print();
   
   //M_jk.Print();
@@ -106,14 +210,18 @@ void BBCalCalibration(const char* filename = "replayed_simdigtest_2.root")
   TMatrixD C_j = M_jk_inv*B_j;
   
   cout << endl;
-  int k = 0;
+  k = 0;
   //C_j.Print();
   cout << " *** add the following lines in your bigbite shower database *** " << endl;
   cout << endl;
   cout << "bb.sh.adc.gain = " << endl;
   for(int i = 0; i<27; i++){
     for(int j = 0; j<7; j++){
-      cout << abs(C_j(k, 0)) << "  ";
+      if(badcells[k]){
+	cout << 1.0 << "  ";
+      }else{
+	cout << abs(C_j(k, 0)) << "  ";
+      }
       k++;
     }
     cout << endl;
@@ -125,13 +233,45 @@ void BBCalCalibration(const char* filename = "replayed_simdigtest_2.root")
   cout << "bb.ps.adc.gain = " << endl;
   for(int i = 0; i<26; i++){
     for(int j = 0; j<2; j++){
-      cout << abs(C_j(k, 0)) << "  ";
+      if(badcells[k]){
+	cout << 1.0 << "  ";
+      }else{
+	cout << abs(C_j(k, 0)) << "  ";
+      }
       k++;
     }
     cout << endl;
   }
   cout << endl;
   
+  //from hcal script: Waht would Etarg be???
+  //xErr[i] = E_targ*sqrt(fabs(M(i,i)));
+  k = 0;
+  cout << "Shower calibration coefficients with errors= " << endl;
+  for(int i = 0; i<27; i++){
+    for(int j = 0; j<7; j++){
+      if(badcells[k]){
+	cout << 1.0 << " +- " << 1.0 << endl;
+      }else{
+	cout << abs(C_j(k, 0)) << " +- "<< sqrt(fabs(M_jk(k,k))) << endl;
+      }
+      k++;
+    }
+  }
+  cout << endl;  
+
+  cout << "PreShower calibration coefficients with errors= " << endl;
+  for(int i = 0; i<26; i++){
+    for(int j = 0; j<2; j++){
+      if(badcells[k]){
+	cout << 1.0 << " +- " << 1.0 << endl;
+      }else{
+	cout << abs(C_j(k, 0)) << " +- "<< sqrt(fabs(M_jk(k,k))) << endl;
+      }
+      k++;
+    }
+  }
+  cout << endl;  
   /*
   TCanvas* C0 = new TCanvas();
   h1_DeltaE->Draw();
