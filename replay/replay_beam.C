db73e519f5ab9f1deffcd04a0a1db33515cf9ad7
David Flay <flay@jlab.org>
2021-11-16 15:30:23 -0500

    Updating beam replay script

--- replay/replay_beam.C
+++ replay/replay_beam.C
@@ -35,21 +35,88 @@ void replay_beam(int runNum){
    unsigned int firstEv = 1; 
    unsigned int lastEv  = 50000; 
 
-   char codaFilePath[200]; 
-   sprintf(codaFilePath,"/adaqeb1/data1/e1209019_%d.evio.0.0",runNum); 
+   // search paths to find the raw data
+   std::vector<TString> pathlist;
+   pathlist.push_back( prefix );
 
-   char outfileName[200]; 
-   sprintf(outfileName,"test_%d.root",runNum); 
+   if(prefix!="/adaqeb1/data1" )  pathlist.push_back( "/adaqeb1/data1" );
+   if(prefix!="/adaq1/data1/sbs") pathlist.push_back( "/adaq1/data1/sbs" );
+   if(prefix!="/cache/mss/halla/sbs/raw") pathlist.push_back( "/cache/mss/halla/sbs/raw" );
 
+   for( int i=0; i<pathlist.size(); i++ ){
+      std::cout << "search paths = " << pathlist[i] << std::endl;
+   }
+
+   //This loop adds all file segments found to the list of THaRuns to process:
+   while( segcounter < max1 && segment - firstsegment < maxsegments ){
+      TString codafilename;
+      //codafilename.Form( "%s/bbgem_%d.evio.%d", prefix.Data(), runnum, segment );
+      codafilename.Form("%s_%d.evio.%d.%d", fname_prefix, runnum, stream, segment );
+      
+      TString ftest(fname_prefix);
+      if( ftest == "bbgem" || ftest == "e1209019_trigtest" ){
+              codafilename.Form("%s_%d.evio.%d", fname_prefix, runnum, segment );
+      }
+      
+      segmentexists = false;
+      
+      cout << "codafilename = " << codafilename << endl;
+      
+      for( int ipath=0; ipath<pathlist.size(); ipath++ ){
+         TString searchname;
+         searchname.Form( "%s/%s", pathlist[ipath].Data(), codafilename.Data() );
+         if( !gSystem->AccessPathName( searchname.Data() ) ){
+            segmentexists = true;
+            break;
+         }
+      }
+      
+      if( segmentexists ){
+         new( (*filelist)[segcounter] ) THaRun( pathlist, codafilename.Data(), "GMN run" );
+         cout << "Added segment " << segment << ", CODA file name = " << codafilename << endl;
+      }
+      if( segmentexists ){
+         segcounter++;
+         lastsegment = segment;
+      }
+      segment++;
+   }
+
+   std::cout << "n segments to analyze = " << segcounter << endl;
+   
    // set up file paths
 
+   prefix = gSystem->Getenv("OUT_DIR");
+
+   // char codaFilePath[200]; 
+   // sprintf(codaFilePath,"/adaqeb1/data1/e1209019_%d.evio.0.0",runNum); 
+
+   // char outfileName[200]; 
+   // sprintf(outfileName,"test_%d.root",runNum);
+   
    // output and cut definition files
-   TString replayDir = gSystem->Getenv("SBS_REPLAY");
-   TString odef_path = Form("%s/replay/output_beam_both.def",replayDir.Data());
-   TString cdef_path = Form("%s/replay/cuts_beam_raster.def",replayDir.Data()); 
+   // TString replayDir = gSystem->Getenv("SBS_REPLAY");
+   // TString odef_path = Form("%s/replay/output_beam_both.def",replayDir.Data());
+   // TString cdef_path = Form("%s/replay/cuts_beam_raster.def",replayDir.Data());
+
+   TString outfilename;
+   outfilename.Form( "%s/gmn_replayed_%d_stream%d_seg%d_%d.root", prefix.Data(), runnum,
+		   stream, firstsegment, lastsegment ); 
+
+   prefix = gSystem->Getenv("LOG_DIR");
+   analyzer->SetSummaryFile(Form("%s/replay_beam_gmn.log", prefix.Data()));
+
+   prefix = gSystem->Getenv("SBS_REPLAY");
+   prefix += "/replay/";
+
+   TString odef_filename = "replay_beam_gmn.odef";
+
+   odef_filename.Prepend( prefix );
+
+   analyzer->SetOdefFile( odef_filename );
    
    // output ROOT file destination and name
-   TString out_file = Form("%s",outfileName);
+   // TString out_file = Form("%s",outfileName);
  
    THaEvent* event = new THaEvent;
 
@@ -109,8 +176,8 @@ void replay_beam(int runNum){
   if(firstEv>0) run->SetFirstEvent(firstEv);
   if(lastEv>0)  run->SetLastEvent(lastEv);
   
-  analyzer->SetOutFile( out_file.Data() );
-  std::cout << SCRIPT << "Output file " << out_file.Data() << " set up " << std::endl; 
+  analyzer->SetOutFile( outfilename.Data() );
+  std::cout << SCRIPT << "Output file " << outfilename.Data() << " set up " << std::endl; 
 
   // File to record cuts accounting information
   analyzer->SetSummaryFile("sbs_beam_test.log"); // optional
