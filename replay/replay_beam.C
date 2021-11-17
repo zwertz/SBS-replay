5f9cb519cee42f6a1353e1cb7afcbaa52362ecfb
David Flay <flay@jlab.org>
2021-11-17 18:45:27 -0500

    More tweaks to beam replay.  Still not working.

--- replay/replay_beam.C
+++ replay/replay_beam.C
@@ -47,13 +47,52 @@ void replay_beam(int runNum,Long_t firstevent=0,Long_t nevents=-1,int maxsegment
    }else{
       analyzer = new THaAnalyzer;
    }
+ 
+   THaEvent* event = new THaEvent;
 
-   // unsigned int firstEv = 1; 
-   // unsigned int lastEv  = 50000;
+   // add the LHRS (where the beam signals are)  
+   THaHRS* HRSL = new THaHRS("L","Left arm HRS");
+   HRSL->AutoStandardDetectors(kFALSE);
+   gHaApps->Add( HRSL );
+
+   // add decoder
+   THaApparatus* decL = new THaDecData("DL","Misc. Decoder Data");
+   gHaApps->Add( decL );
+
+   // add *rastered* beam (LHRS)
+   THaApparatus* Lrb = new SBSRasteredBeam("Lrb","Raster Beamline for FADC");
+   gHaApps->Add(Lrb);
+
+   // add *rastered* beam (SBS)
+   THaApparatus* sbs = new SBSRasteredBeam("SBSrb","Raster Beamline for FADC");
+   gHaApps->Add(sbs);
+
+   std::ofstream debugFile; 
+   debugFile.open("lhrs-scaler-dump.txt");
+
+   // LHRS scaler data (ROC10)  
+   LHRSScalerEvtHandler *lScaler = new LHRSScalerEvtHandler("Left","HA scaler event type 140");
+   lScaler->SetDebugFile(&debugFile);
+   gHaEvtHandlers->Add(lScaler);
+
+   // SBS scaler data (sbsvme29) 
+   SBSScalerEvtHandler *sbsScaler = new SBSScalerEvtHandler("sbs","SBS Scaler Bank event type 1");
+   // sbsScaler->AddEvType(1);             // Repeat for each event type with scaler banks
+   sbsScaler->SetUseFirstEvent(kTRUE);
+   gHaEvtHandlers->Add(sbsScaler);
+
+   analyzer->SetEvent(event);
+   analyzer->SetCompressionLevel(1);
+
+   // File to record cuts accounting information
+   analyzer->SetSummaryFile("sbs_beam_test.log"); // optional
+
+   analyzer->SetVerbosity(10);   
+
+   // find all files associated with the run 
   
    TString prefix = gSystem->Getenv("DATA_DIR");
 
-   // find all files associated with the run 
    char fname_prefix[200]; 
    sprintf(fname_prefix,"e1209019"); 
 
@@ -73,11 +112,10 @@ void replay_beam(int runNum,Long_t firstevent=0,Long_t nevents=-1,int maxsegment
       std::cout << "search paths = " << pathlist[i] << std::endl;
    }
 
-   // int maxsegments=1;
    int max1 = maxsegments;
    int segcounter=0,segment=0,firstsegment=0,lastsegment=0; 
 
-   bool segmentexists;
+   bool segmentexists=false;
 
    //This loop adds all file segments found to the list of THaRuns to process:
    while( segcounter < max1 && segment - firstsegment < maxsegments ){
@@ -87,7 +125,7 @@ void replay_beam(int runNum,Long_t firstevent=0,Long_t nevents=-1,int maxsegment
       
       TString ftest(fname_prefix);
       if( ftest == "bbgem" || ftest == "e1209019_trigtest" ){
-              codafilename.Form("%s_%d.evio.%d", fname_prefix, runNum, segment );
+         codafilename.Form("%s_%d.evio.%d", fname_prefix, runNum, segment );
       }
       
       segmentexists = false;
@@ -131,59 +169,14 @@ void replay_beam(int runNum,Long_t firstevent=0,Long_t nevents=-1,int maxsegment
    TString odef_filename = Form("%s/output_beam_both.def",prefix.Data());
    TString cdef_filename = Form("%s/cuts_beam_raster.def",prefix.Data());
 
-   odef_filename.Prepend( prefix );
-
    analyzer->SetOdefFile(odef_filename);
-   analyzer->SetCutFile(cdef_filename);
-   
-   THaEvent* event = new THaEvent;
-
-   // add the LHRS (where the beam signals are)  
-   THaHRS* HRSL = new THaHRS("L","Left arm HRS");
-   HRSL->AutoStandardDetectors(kFALSE);
-   gHaApps->Add( HRSL );
-
-   // add decoder
-   THaApparatus* decL = new THaDecData("DL","Misc. Decoder Data");
-   gHaApps->Add( decL );
-
-   // add *rastered* beam (LHRS)
-   THaApparatus* Lrb = new SBSRasteredBeam("Lrb","Raster Beamline for FADC");
-   gHaApps->Add(Lrb);
-
-   // add *rastered* beam (SBS)
-   THaApparatus* sbs = new SBSRasteredBeam("SBSrb","Raster Beamline for FADC");
-   gHaApps->Add(sbs);
-
-   std::ofstream debugFile; 
-   debugFile.open("lhrs-scaler-dump.txt");
-
-   // LHRS scaler data (ROC10)  
-   LHRSScalerEvtHandler *lScaler = new LHRSScalerEvtHandler("Left","HA scaler event type 140");
-   lScaler->SetDebugFile(&debugFile);
-   gHaEvtHandlers->Add(lScaler);
-
-   // SBS scaler data (sbsvme29) 
-   SBSScalerEvtHandler *sbsScaler = new SBSScalerEvtHandler("sbs","SBS Scaler Bank event type 1");
-   // sbsScaler->AddEvType(1);             // Repeat for each event type with scaler banks
-   sbsScaler->SetUseFirstEvent(kTRUE);
-   gHaEvtHandlers->Add(sbsScaler);
-
-   analyzer->SetEvent(event);
-
-   analyzer->SetCompressionLevel(1);
-   // analyzer->SetOdefFile(odef_path.Data());
-   // analyzer->SetCutFile(cdef_path.Data());
-   
-   analyzer->SetOutFile( outfilename.Data() );
+   analyzer->SetCutFile(cdef_filename);      
+   analyzer->SetOutFile(outfilename);
    std::cout << SCRIPT << "Output file " << outfilename.Data() << " set up " << std::endl; 
-
-   // File to record cuts accounting information
-   analyzer->SetSummaryFile("sbs_beam_test.log"); // optional
-
-   analyzer->SetVerbosity(10);   
-
+  
    filelist->Compress();
+
+   int runSegment=-1;
 
    for(int iseg=0; iseg<filelist->GetEntries(); iseg++ ){
       std::cout << SCRIPT << "Trying file segment " << iseg << std::endl;
@@ -194,8 +187,10 @@ void replay_beam(int runNum,Long_t firstevent=0,Long_t nevents=-1,int maxsegment
       // FIXME: to address prescale factor issue (temporary) 
       // TDatime now; 
       // run->SetDate(now); 
-      // run->SetDataRequired(0); 
-      if( run->GetSegment() >= firstsegment && run->GetSegment() - firstsegment < maxsegments ){
+      // run->SetDataRequired(0);
+      runSegment = run->GetSegment(); 
+      std::cout << "   run segment: " << runSegment << std::endl;  
+      if( runSegment>=firstsegment && (runSegment-firstsegment)<maxsegments ){
          analyzer->Process(run);     // start the actual analysis
          std::cout << SCRIPT << "--> Done!" << std::endl;
       }
