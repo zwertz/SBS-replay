3425f4bad44ea2805336502daf7dd78503992a63
Sebastian Seeds <sseeds@ifarm1802.jlab.org>
2022-01-24 11:06:09 -0500

    Fixed panguin plots to read the sbs.hcal.clus_blk.* branches which are still on the tree.

--- onlineGUIconfig/hcal_ADCTime_LED.C
+++ onlineGUIconfig/hcal_ADCTime_LED.C
@@ -5,7 +5,7 @@ void hcal_ADCTime_LED(){
   TStopwatch *st = new TStopwatch();
   st->Start(kTRUE);
 
-  Double_t sbs_hcal_clus_e[30] = {0.}, sbs_hcal_clus_atime[30] = {0.}, sbs_hcal_nclus = 0.; 
+  Double_t sbs_hcal_clus_blk_e[30] = {0.}, sbs_hcal_clus_blk_atime[30] = {0.}, sbs_hcal_nclus = 0.; 
   UInt_t fEvtHdr_fTrigBits = -1;
 
   Double_t sbs_hcal_ledbit = 0.;
@@ -15,14 +15,14 @@ void hcal_ADCTime_LED(){
   // Declare branches
   TTree *T = (TTree*) gDirectory->Get("T");
   T->SetBranchStatus("*",0);
-  T->SetBranchStatus("sbs.hcal.clus.atime",1);
-  T->SetBranchStatus("sbs.hcal.clus.e",1);
+  T->SetBranchStatus("sbs.hcal.clus_blk.atime",1);
+  T->SetBranchStatus("sbs.hcal.clus_blk.e",1);
   T->SetBranchStatus("sbs.hcal.nclus",1);
   T->SetBranchStatus("fEvtHdr.fTrigBits",1);
   T->SetBranchStatus("sbs.hcal.ledbit",1);
 
-  T->SetBranchAddress("sbs.hcal.clus.atime", &sbs_hcal_clus_atime);
-  T->SetBranchAddress("sbs.hcal.clus.e", &sbs_hcal_clus_e);
+  T->SetBranchAddress("sbs.hcal.clus_blk.atime", &sbs_hcal_clus_blk_atime);
+  T->SetBranchAddress("sbs.hcal.clus_blk.e", &sbs_hcal_clus_blk_e);
   T->SetBranchAddress("sbs.hcal.nclus", &sbs_hcal_nclus);
   T->SetBranchAddress("fEvtHdr.fTrigBits", &fEvtHdr_fTrigBits);
   T->SetBranchAddress("sbs.hcal.ledbit", &sbs_hcal_ledbit);
@@ -34,11 +34,11 @@ void hcal_ADCTime_LED(){
 
     T->GetEntry(nevent);
     //sbs_hcal_clus_e[0]<0.35
-    if( sbs_hcal_clus_atime[0]<1 || sbs_hcal_nclus==0 ) continue;
+    if( sbs_hcal_clus_blk_atime[0]<1 || sbs_hcal_nclus==0 ) continue;
 
     if(fEvtHdr_fTrigBits==32) {
     //if(sbs_hcal_ledbit!=-1){
-      h1_hcal_adctime_led->Fill( sbs_hcal_clus_atime[0] );
+      h1_hcal_adctime_led->Fill( sbs_hcal_clus_blk_atime[0] );
     }
   }
   
